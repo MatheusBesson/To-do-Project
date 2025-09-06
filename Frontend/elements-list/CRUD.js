1bc7ac301c58635e31a3b5f71f463b20b9d08b92
Matheus Besson <matheusbesson.main@gmail.com>
2025-09-06 19:51:40 -0300

    arrumar app.js

--- Frontend/elements-list/CRUD.js
+++ Frontend/elements-list/CRUD.js
@@ -1,14 +1,12 @@
+
 import { API_URL } from "../script/config/config";
-import { addOneToDo } from "./todos-list";
 
 export function createToDo() {
 
     const form = document.getElementById('todo-form');
 
-
-    form.addEventListener("submit", async (e) => {
-        e.preventDefault(); // no page reload
-
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
 
         const title = document.getElementById("title").value;
         const description = document.getElementById("description").value;
@@ -16,22 +14,24 @@ export function createToDo() {
         try {
             const response = await fetch(API_URL, {
                 method: "POST",
-                headers: {"Content-Type": "application/json"},
+                headers: {
+                    "Content-Type": "application/json"
+                },
                 body: JSON.stringify({
-                    title: title, 
-                    description: description, 
-                    status: false})
+                    title: title,
+                    description: description,
+                    status: false
+                })
             });
 
-            if(!response.ok) throw new Error('Error when creating ToDo');
+            if (!response.ok) {
+                throw new Error("Erro ao criar ToDo");
+            }
 
-            const result = await response.json();
-            console.log(result);
-            
-        } catch (error){
-            console.log(error);
+        } catch (error) {
+            console.error("Erro na requisição:", error);
         }
-    })
-}
 
-// changestatus button function (toggleStatus)
+    })
+
+}
--- Frontend/script/app.js
+++ Frontend/script/app.js
@@ -1,4 +1,5 @@
 
+import { API_URL } from "./config/config.js";
 import { fetchTodos } from "./Connection/api.js";
 
 
@@ -6,9 +7,44 @@ import { fetchTodos } from "./Connection/api.js";
 
 function startApp() {
 fetchTodos();
-
+createToDo();
 
 
 }
 
 startApp();
+
+function createToDo() {
+
+    const form = document.getElementById('todo-form');
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const title = document.getElementById("title").value;
+        const description = document.getElementById("description").value;
+
+        try {
+            const response = await fetch(API_URL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    title: title,
+                    description: description,
+                    status: false
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error("Erro ao criar ToDo");
+            }
+
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+        }
+        fetchTodos();
+    })
+
+}
